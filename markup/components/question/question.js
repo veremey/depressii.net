260f768815597b4b42196526b9ef7e5e7c278ccd
Veremey <veremey4uk@gmail.com>
2018-04-22 13:21:53 +0300

    #1

--- markup/components/question/question.js
+++ markup/components/question/question.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function () {
 
+
 	$('.meter-ui').slider({
       range: "min",
       min: 0,
@@ -18,11 +19,15 @@ $(document).ready(function () {
   $('.btn-back').on('click', prevTest);
 
 
+
 });//docready
 
+var index = 0;
 var $value = 1;
+var points = 0;
+var answer = [];
 
-function getFirstPage(val) {
+function getPageNum(val) {
 	switch (val) {
 		case 1:
 			$('#pre-question').slideDown();
@@ -36,8 +41,26 @@ function getFirstPage(val) {
 			$('.btn-next').html('<span>Посмотреть результаты</span><i class="icon-help"></i>');
 			break;
 		case 15:
+			for (var i = answer.length - 1; i >= 0; i--) {
+				points += answer[i];
+			}
+
+		  if(points < 15){
+		  	index = 0;
+		  }else if(15 > points > 21) {
+		  	index = 1;
+		  } else {
+		  	index = 2;
+		  }
+
+			var buildChart = initChart();
+
+			$('.result__diagramm_points').each(function () {
+				$(this).text(points);
+			});
 			$('.js-question-section').hide();
 			$('.js-result-section').show();
+			$('.result').eq(index).addClass('is-active');
 			break;
 		default:
 			break;
@@ -46,11 +69,16 @@ function getFirstPage(val) {
 
 function nextTest(e) {
 	e.preventDefault();
+	var value = $('#question-' + $value).find('.radio__input:checked').data('point');
+	answer.push(+value);
+	// console.log(answer);
+
 	var newValue = ++$value ;
 	var slideQuestion = spinQuestion(newValue);
 }
 
 function prevTest() {
+	answer.pop();
 	var newValue = --$value;
 	var slideQuestion = spinQuestion(newValue);
 }
@@ -61,5 +89,33 @@ function spinQuestion(arg) {
 	$('#question-'+arg).addClass('is-active');
 	$( ".meter__start" ).text( $( ".meter-ui" ).slider( "value" ) );
 	$( ".title__big span" ).text( $( ".meter-ui" ).slider( "value" ) );
-	var page = getFirstPage(arg);
+	var page = getPageNum(arg);
+}
+
+function initChart() {
+  var value = 42 - points;
+
+  var data = {
+    datasets: [{
+    		backgroundColor: ['#004e73' ,'#d2d2d2'],
+        data: [points, value]
+    }],
+	};
+
+	var ctx = $('.result__chart').get(index).getContext('2d');
+
+  var chart = new Chart(ctx, {
+    type: 'doughnut',
+    data: data,
+    options: {
+      	tooltips: {
+      		enabled: false
+      	},
+      	legend: {
+      	  display: false,
+      	},
+      	cutoutPercentage: 64
+      }
+  });
+
 }
